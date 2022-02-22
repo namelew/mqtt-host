2d19fba9ad080642d0fa637f8d558d5b7630245d
“namelew” <diogomaciel.cunha@gmail.com>
2022-02-22 14:59:09 -0300

    imports de bibliotecas para fazer as conexões

--- connection.go
+++ connection.go
@@ -1,4 +1,11 @@
 // esse arquivo deve conter todos os métodos relacionados a conexão aos clientes dessa aplicação
+import (
+	"bufio"
+	"fmt"
+	"net" // baixar dependência do net
+	"os"
+	"strings"
+)
 
 // conectar()
 
