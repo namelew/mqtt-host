ad825c5ea2abd5dea73bc8307610d524ef2996d2
“namelew” <diogomaciel.cunha@gmail.com>
2022-02-22 14:27:45 -0300

    criando estrutura básica do projeto

---
+++ connection.go
@@ -0,0 +1,11 @@
+// esse arquivo deve conter todos os métodos relacionados a conexão aos clientes dessa aplicação
+
+// conectar()
+
+// checarConexão()
+
+// checarRequisições()
+
+// criarResição()
+
+// receberResposta()
---
+++ main.go
@@ -0,0 +1,7 @@
+// main()
+
+// receber paramentros via terminar(como no mqtt-lantency anterior)
+
+// chamar funções do connection.go
+
+// gerar tratar resultados do teste
