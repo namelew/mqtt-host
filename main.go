236851ed35bec28e5cfa2af55cb8c39c716a9570
namelew <diogomaciel.cunha@gmail.com>
2022-03-24 13:30:46 -0700

    primeira estrutura do orquestrador

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ import (
 	"log"
 	"time"
 	"strconv"
+	"os"
 )
 
 import (
@@ -28,13 +29,31 @@ func main(){
 		format    = flag.String("format", "text", "Output format: text|json")
 		quiet     = flag.Bool("quiet", false, "Suppress logs while running")
 		frequency = flag.Int("frequency", 1, "Number of publications per second")
+		benckmark = flag.Int("benckmark", 1, "Selected benckmark software:\n 1 - Soft 1\n 2 - Soft 2")
 	)
 
 	flag.Parse()
-    ka, _ := time.ParseDuration(strconv.Itoa(*keepalive) + "s")
+
+	commands := []string {
+		*broker,
+		*topic,
+		string(*fin),
+		string(*fout),
+		*username,
+		*password,
+		string(*pubqos),
+		string(*subqos),
+		string(*size),
+		string(*count),
+		string(*clients),
+		string(*keepalive),
+		*format,
+		string(*quiet),
+		string(*frequency),
+		string(*benckmark)
+	}
 
     opts := mqtt.NewClientOptions().AddBroker(*broker).SetAutoReconnect(true).SetKeepAlive(ka).SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
-        // ler mensagem e achar canal de envio de comandos e canal de recebimento de resultados
     }).SetConnectionLostHandler(func(client mqtt.Client, reason error) {
         log.Printf("SUBSCRIBER Orquestrador lost connection to the broker: %v. Will reconnect...\n", reason.Error())
     })
@@ -43,15 +62,23 @@ func main(){
 
     if token := client.Connect(); token.Wait() && token.Error() != nil {
 		log.Printf("SUBSCRIBER Orquestrador had error connecting to the broker: %v\n", token.Error())
-        // exit
+        os.Exit(3)
 	}
 
-	if token := client.Subscribe("status_clients", byte(1), nil); token.Wait() && token.Error() != nil {
-		log.Printf("SUBSCRIBER Orquestrador had error subscribe with topic: %v\n", token.Error())
-		// exit
-	}
+	token := client.Subscribe("status_clients", byte(1), func(Client client, Message m) {
+		// [0:8] command, [8:16] result 
+		message := string(m.Payload())
+		commad_channel := message[0:8]
+		resul_channel := message[8:16]
 
-    // enviar commandos aos cliente encontrados
-    // esperar respostas e calcular resultados
+		var command_message := strings.Join(commands," ")
 
+		// enviar comando
+		client.Publish(commad_channel, byte(1), true, command_message)
+		// se inscrever no canal que o cliente enviou
+		client.Subscribe(resul_channel, byte(1), func(Client client, Message m){
+			result := string(m.Payload())
+			// adicionar em uma estrutura de resultados
+		})
+	}) // ver se eh isso mesmo
 }
