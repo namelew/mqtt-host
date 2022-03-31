4ce6c6c10e6f02de5cbaba60121f14a1fcb85f7f
“namelew” <diogomaciel.cunha@gmail.com>
2022-03-31 15:32:47 -0300

    integrando com esboço de rotina do orquestrador

--- main.go
+++ main.go
@@ -11,49 +11,41 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
+func RemoveIndex(s []string, index int) []string {
+    return append(s[:index], s[index+1:]...)
+}
 
 func main(){
-	// ler arquivo de configurações
-
 	var (
-		broker    = flag.String("broker", "tcp://localhost:1883", "MQTT broker endpoint as scheme://host:port")
-		topic     = flag.String("topic", "/test", "MQTT topic for outgoing messages")
-		fin       = flag.Bool("fin", false, "Allows fan-in tests")
-		fout      = flag.Bool("fout", false, "Allows fan-out tests")
-		username  = flag.String("username", "", "MQTT username (empty if auth disabled)")
-		password  = flag.String("password", "", "MQTT password (empty if auth disabled)")
-		pubqos    = flag.Int("pubqos", 1, "QoS for published messages")
-		subqos    = flag.Int("subqos", 1, "QoS for subscribed messages")
-		size      = flag.Int("size", 100, "Size of the messages payload (bytes)")
-		count     = flag.Int("count", 100, "Number of messages to send per pubclient")
-		clients   = flag.Int("clients", 10, "Number of clients pair to start")
-		keepalive = flag.Int("keepalive", 60, "Keep alive period in seconds")
-		format    = flag.String("format", "text", "Output format: text|json")
-		quiet     = flag.Bool("quiet", false, "Suppress logs while running")
-		frequency = flag.Int("frequency", 1, "Number of publications per second")
-		benckmark = flag.Int("benckmark", 1, "Selected benckmark software:\n 1 - Soft 1\n 2 - Soft 2")
+		path = flag.String("path", "commands.config", "open a file with the commands to the app")
 	)
 
 	flag.Parse()
 
-	// formato terá que mudar
-	commands := []string {
-		*broker,
-		*topic,
-		string(*fin),
-		string(*fout),
-		*username,
-		*password,
-		string(*pubqos),
-		string(*subqos),
-		string(*size),
-		string(*count),
-		string(*clients),
-		string(*keepalive),
-		*format,
-		string(*quiet),
-		string(*frequency),
-		string(*benckmark)
+	extInput := filepath.Ext(*path)
+
+	if (extInput == ".config"){
+		content, _ := ioutil.ReadFile(*path)
+		data := strings.Split(string(content), "\n")
+		oq_commands := ""
+		
+		for i:=0; i < len(data); i++{
+			if strings.Contains(data[i], "//"){
+				data = RemoveIndex(data, i)
+			}
+		}
+
+		for i:=0; i < len(data); i++{
+			if strings.Contains(data[i], "@"){
+				oq_commands += data[i] + " "
+				data = RemoveIndex(data, i)
+				i -= 1 
+			}
+		}
+		
+		commands := strings.Join(data," ")
+	}else{
+		fmt.Print("Erro ao abrir arquivo")
 	}
 
     opts := mqtt.NewClientOptions().AddBroker(*broker).SetAutoReconnect(true).SetKeepAlive(ka).SetDefaultPublishHandler(func(client mqtt.Client, msg mqtt.Message) {
@@ -74,10 +66,8 @@ func main(){
 		commad_channel := message[0:8]
 		resul_channel := message[8:16]
 
-		var command_message := strings.Join(commands," ")
-
 		// enviar comando
-		client.Publish(commad_channel, byte(1), true, command_message)
+		client.Publish(commad_channel, byte(1), true, commands)
 		// se inscrever no canal que o cliente enviou
 		client.Subscribe(resul_channel, byte(1), func(Client client, Message m){
 			result := string(m.Payload())
